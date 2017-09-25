20b514936273b4d5c40b195fb31377d12adde980
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-25 21:51:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,65 +1,91 @@
 
 module.exports = function zeros(expression) 
 {
-var factorial = function(number) 
+var factorial = function(number) //считаем нули у !
 {
- if (number <= 0) { // terminal case
- return 1;
- } else { // block to execute
- return (number * factorial(number - 1));
- }
+  var shet=0;
+  var x=2;
+  var y=5;
+for(var i=0; i<number; i++)
+{
+  if(number%x==0)
+    { 
+    shet=shet+(number/x);
+    }
+    x=x*2;
+}
+for(var i=0; i<number; i++)
+{
+  if(number%y==0)
+    { 
+    shet=shet+(number/y);
+    }
+    y=y*5;
+
+
+}alert(shet); return shet;
 }
 
-var factorial_2 = function(number) 
+
+var factorial_2 = function(number) //считаем нули у !!
 {
- if (number <= 0)   {  return 1; } 
- else  {  return (number * factorial_2(number - 2)); }
+  var shet=0;
+  var x=2;
+  var y=10;
+for(var i=0; i<number; i++)
+{
+  if(number%x==0)
+    { 
+    shet=shet+(number/x);
+    }
+    x=x*2;
 }
+for(var i=0; i<number; i++)
+{
+  if(number%y==0)
+    { 
+    shet=shet+(number/y);
+    }
+    y=y*10;
+
+
+}alert(shet); return shet;
+}
+
+
+
+
+expression='45!*28!*63!';
 var mass=expression.split('*'); //разбили строку на массив без *
-
 var newmass = mass.map(function(name) //каждый элемент откинули все кроме цифр
-{
-  return name.replace(/\D/g,'');
-});
+{  return name.replace(/\D/g,'');});
 
-
-//alert( newmass );// получили массив с числами
+//alert( newmass +" massiv hisel");// получили массив с числами
 //alert(mass[0].length);//длинна строки элемента
-//alert(mass.length);//кол-во элементов массива
+//alert(mass.length + " kolihestvo elem mass ");
 var lastmass=[];
-for(var i=0;i<mass.length;i++)
-{
-  if((mass[i].length-newmass[i].length)==1)
-  { newmass[i]= +newmass[i];
-    lastmass[i]=factorial(newmass[i]);
+for(var i=0;i<mass.length;i++)//кол-во элементов массива пробегаем
+{  
+  if((mass[i].length-newmass[i].length)==1)//если факториал один
+  { 
+    newmass[i]= +newmass[i];
+    lastmass[i]=factorial(newmass[i]);//считаем нули каждого элемента массива
 
   }
-  else
-   {
+  else // если факториала два
+   { if ((newmass[i]%2)==0){
     newmass[i]= +newmass[i];
-    lastmass[i]=factorial_2(newmass[i]);
+    lastmass[i]=factorial_2(newmass[i]);}
   }
 }
-//alert( lastmass );//возведенные в факториал числа массив 120 3 24 48
-var mult=1;
+
+alert( lastmass );//массив из количества нулей в каждом факториале
+var summ=0;
 for(var i=0;i<lastmass.length;i++)
 {
- mult=mult*lastmass[i];
+ summ=summ+lastmass[i];
 }
-//alert(mult);// 414720
-var lastnum, zeross=0;
-var n=true;
-
-while(n)
-{  
-  lastnum = mult % 10; /* находим остаток от деления на 10 */
-    if(lastnum==0)
-    {
-    zeross++;
-    mult=mult/10;
-    }
-    else{ n=false;}
-}
-return(zeross);
+alert(summ);// 414720
+return summ;
 
 }
